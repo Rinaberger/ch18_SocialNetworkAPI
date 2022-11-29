29f00193e65baf6c6dbba2cbd946c85b74d5033a
Scott Rinaberger <scottrinaberger@gmail.com>
2022-11-29 11:06:32 -0600

    initial thought model code

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+//thought schema
+
+const ThoughtSchema = new Schema(
+    {
+        thoughText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionShema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought", ThoughtSchema);
+  
+  module.exports = Thought;
